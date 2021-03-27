b977d965aba78ef950dff3b30f05cd555aed078f
haeun-Digipen <80096463+haeun-Digipen@users.noreply.github.com>
2021-03-27 15:23:16 +0900

    add Object class

---
+++ doodle/Engine/Object.h
@@ -0,0 +1,36 @@
+/*--------------------------------------------------------------
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name: Object.h
+Project: GAM150_Networker
+Author: Team RETRY - Yujin Park / Yeongju Lee / Haeun Park
+Creation date: 03/27/2021
+-----------------------------------------------------------------*/
+#pragma once
+#include "Vec2.h"
+#include <doodle/environment.hpp> 
+#include <doodle/drawing.hpp> 
+
+namespace Retry
+{
+	class Object
+	{
+	private:
+		math::vec2 position;
+		double width;
+		double height;
+
+	public:
+		Object(math::vec2 pos, double w, double h) : position(pos), width(w), height(h) {}
+		~Object() {}
+
+		virtual void Update() = 0;
+		virtual void Draw() const = 0;
+
+		math::vec2 GetPosition() { return position; }
+		double GetWidth() { return width; }
+		double GetHeight() { return height; }
+	};
+}
+
