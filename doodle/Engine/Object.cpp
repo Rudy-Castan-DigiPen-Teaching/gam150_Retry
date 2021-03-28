7bc2b1374782fd6765200e3653fcd3013bbec9db
Yeongju Lee <vntvnt6107@gmail.com>
2021-03-28 17:38:01 +0900

    Implemented Collision check, but it need to be fixed

---
+++ doodle/Engine/Object.cpp
@@ -0,0 +1,56 @@
+#include "Engine.h"
+#include "Object.h"
+#include <doodle/drawing.hpp>
+
+namespace Retry
+{
+	void Object::Update() {}
+
+	void Object::Draw() const
+	{
+		doodle::push_settings();
+		doodle::draw_rectangle(position.x - hotspot.x, position.y - hotspot.y, width, height);
+		doodle::pop_settings();
+	}
+
+	bool Object::CollideWith(Object& obj)
+	{
+		math::vec2 corner = position - hotspot;
+		math::vec2 objCorner = obj.GetPosition() - obj.GetHotspot();
+
+		if (position.x + width >= objCorner.x &&
+			position.x <= objCorner.x + obj.GetSize().x &&
+			position.y + height >= objCorner.y &&
+			position.y <= objCorner.y + obj.GetSize().y)
+		{
+			return true;
+		}
+		return false;
+	}
+}
+
+
+
+
+
+/*
+
+class DataaBox : public Object
+{
+private:
+	DataKind data;
+
+public:
+	enum class DataKind {
+		
+		red,
+		green,
+		blue
+
+	}
+
+	DataaBox(DataKind data) : data(data) {}
+
+
+
+*/
---
+++ doodle/Game/TestObject.cpp
@@ -0,0 +1,18 @@
+#include "TestObject.h"
+#include <doodle/drawing.hpp>
+#include <doodle/input.hpp>
+
+TestObject::TestObject(math::vec2 initpos, double size) : Object(initpos, size),
+moveUpKey(Retry::InputKey::Keyboard::W), moveDownKey(Retry::InputKey::Keyboard::S), moveLeftKey(Retry::InputKey::Keyboard::A), moveRightKey(Retry::InputKey::Keyboard::D){}
+
+void TestObject::Update() 
+{
+	SetPosition({ static_cast<double>(doodle::get_mouse_x()), static_cast<double>(doodle::get_mouse_y()) });
+}
+
+void TestObject::Draw() const
+{
+	doodle::push_settings();
+	doodle::draw_rectangle(Object::GetPosition().x, Object::GetPosition().y, Object::GetSize().x);
+	doodle::pop_settings();
+}
