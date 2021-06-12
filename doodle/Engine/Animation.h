1d205997ce4cf6b9f489c19a66a3ac9f81ff1c21
Yeongju Lee <vntvnt6107@gmail.com>
2021-06-12 20:49:05 +0900

    Update Animation.h

--- doodle/Engine/Animation.h
+++ doodle/Engine/Animation.h
@@ -1,3 +1,12 @@
+/*--------------------------------------------------------------
+Copyright (C) 2021 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the prior
+written consent of DigiPen Institute of Technology is prohibited.
+File Name: Animation.h
+Project: GAM150_Networker
+Author: Team RETRY - Yujin Park / Yeongju Lee / Haeun Park
+Creation date: 06/03/2021
+-----------------------------------------------------------------*/
 #pragma once
 #include <vector>
 #include <filesystem>
